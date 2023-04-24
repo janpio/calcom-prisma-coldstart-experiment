ba463e3ea4e931dc6f6acd44a882d57dc70c44d0
Jan Piotrowski <piotrowski+github@gmail.com>
2023-04-25 00:42:29 +0200

    move page

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,18 +1,43 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import type { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { PrismaClient, App } from "@prisma/client";
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps<{ apps: (Omit<App, "createdAt" | "updatedAt"> & {
+  createdAt: string;
+  updatedAt: string;
+})[] }> = async (context: GetServerSidePropsContext) => {
+  const prisma = new PrismaClient()
+  const apps = await prisma.app.findMany();
+  return {
+    props: {
+      apps: apps.map( app => ({
+        ...app,
+        createdAt: new Date(app.createdAt).toString(),
+        updatedAt: new Date(app.updatedAt).toString()
+      })),
+    }
+  }
+}
+
+export default function Home({ apps }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>prisma json</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          prsima json
         </h1>
+
+        <p>
+          {apps.map( app => 
+            JSON.stringify(app)
+          )}
+        </p>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
